e226c098d43d5132fe23d03f1883bd34f0716cfe
Jesse Schulman <jesse@caleos.io>
2018-12-07 22:29:12 -0800

    Set CPU limits back to reasonable defaults

--- Launcher.js
+++ Launcher.js
@@ -69,14 +69,14 @@ const globalValues = {
     'max_transaction_lifetime': 3600,
     'deferred_trx_expiration_window': 600,
     'max_authority_depth': 6,
-    'max_transaction_cpu_usage': 4294967194,
+    'max_transaction_cpu_usage': 150000,
     'max_block_net_usage': 1048576,
     'target_block_net_usage_pct': 1000,
     'max_generated_transaction_count': 16,
     'max_inline_action_size': 4096,
     'target_block_cpu_usage_pct': 500,
     'base_per_transaction_net_usage': 12,
-    'max_block_cpu_usage': 4294967295,
+    'max_block_cpu_usage': 200000,
     'max_inline_action_depth': 4
 };
 
